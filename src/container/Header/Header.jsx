b5fb6d44f314767809585fda108ab8727b552733
lohith <lohith05amal@gmail.com>
2022-03-02 21:02:21 -0500

    special menu

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+
+import { SubHeading } from "../../components";
 import { images } from "../../constants";
 import "./Header.css";
-import { SubHeading } from "../../components";
+
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
       <p className="p__opensans" style={{ margin: "2rem 0" }}>
-        {" "}
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla animi
-        aspernatur sequi exercitationem nam similique quae minima, molestiae hic
-        accusamus!
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus{" "}
       </p>
-      <buton type="button" className="custom__button">
+      <button type="button" className="custom__button">
         Explore Menu
-      </buton>
+      </button>
     </div>
+
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="welcome image" />
+      <img src={images.welcome} alt="header_img" />
     </div>
   </div>
 );
